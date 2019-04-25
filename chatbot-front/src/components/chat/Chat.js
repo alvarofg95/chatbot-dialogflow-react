943be9a5d0fad6807d02506c1ab8cb5916ac4587
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-25 14:22:49 +0200

    mantener los mensajes del inicio

--- chatbot-front/src/components/chat/Chat.js
+++ chatbot-front/src/components/chat/Chat.js
@@ -3,22 +3,20 @@ import MessageList from './MessageList';
 import InputText from './InputText';
 import sendMessageToDF from '../../utils/apiCalls';
 
-const MESSAGES_DEFAULT = [
-  {
-    user: 'bot',
-    message: '¡Hola!, soy un asistente virtual'
-  },
-  {
-    user: 'bot',
-    message: 'Espero que te sirva de ayuda'
-  }
-];
-
 export default class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      messages: [
+        {
+          user: 'bot',
+          message: '¡Hola!, soy un asistente virtual'
+        },
+        {
+          user: 'bot',
+          message: 'Espero que te sirva de ayuda'
+        }
+      ]
     };
     this.messages = React.createRef();
     this.sendMessage = this.sendMessage.bind(this);
@@ -48,14 +46,12 @@ export default class Chat extends Component {
   }
 
   render() {
-    const messages =
-      this.state.messages && this.state.messages.length ? this.state.messages : MESSAGES_DEFAULT;
     return (
       <div>
         <span className="botTitle">Chatbot de Álvaro</span>
         <div className="chatContainer">
           <div ref={this.messages} className="messageList">
-            <MessageList messageList={messages} />
+            <MessageList messageList={this.state.messages} />
           </div>
           <InputText sendMessage={this.sendMessage} />
         </div>
